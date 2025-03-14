563245153f046caa5b5b0a2d289283d30667e3b8
QuCoon AI, DA & ML <machinelearningteam@qucoon.com>
2025-03-14 14:45:27 +0100

    Initial commit for InstitutionFeeManager

---
+++ src/main/kotlin/com/pwb/institutionFeeManager/utility/constant/PrefixConstant.kt
@@ -0,0 +1,6 @@
+//TODO ==== BUILT WITH QOONITY VERSION-1.0 - YINKA-ANTHONY-NKAY ON 2025-03-14T13:42:58.656449400
+package com.pwb.institutionFeeManager.utility.constant
+
+object PrefixConstant {
+    const val ENTITY = "DBO."
+}
