9bf6121ff956b5392a63c3c93bd4c6befc4a57a0
QuCoon AI, DA & ML <machinelearningteam@qucoon.com>
2025-03-14 14:45:29 +0100

    Initial commit for InstitutionFeeManager

---
+++ src/main/kotlin/com/pwb/institutionFeeManager/utility/constant/PrivilegeConstant.kt
@@ -0,0 +1,11 @@
+//TODO ==== BUILT WITH QOONITY VERSION-1.0 - YINKA-ANTHONY-NKAY ON 2025-03-14T13:42:58.656449400
+package com.pwb.institutionFeeManager.utility.constant
+
+object PrivilegeConstant {
+    const val CREATE = "CREATE"
+    const val UPDATE = "UPDATE"
+    const val READ = "READ"
+    const val DELETE = "DELETE"
+    const val CHECKER = "CHECKER"
+    const val ALL = "*"
+}
