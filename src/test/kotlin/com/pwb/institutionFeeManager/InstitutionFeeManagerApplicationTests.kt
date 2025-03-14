cf07b08e623976f1977369d91f33167f873d8424
QuCoon AI, DA & ML <machinelearningteam@qucoon.com>
2025-03-14 14:45:56 +0100

    Initial commit for InstitutionFeeManager

---
+++ src/test/kotlin/com/pwb/institutionFeeManager/InstitutionFeeManagerApplicationTests.kt
@@ -0,0 +1,15 @@
+//TODO ==== BUILT WITH QOONITY VERSION-1.0 - YINKA-ANTHONY-NKAY ON 2025-03-14T13:42:58.720367100
+package com.pwb.institutionFeeManager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InstitutionFeeManagerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
+
